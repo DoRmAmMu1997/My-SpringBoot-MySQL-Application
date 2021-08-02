1ca0d2489f24d105cac85b2e29a4ac43b5d3bb58
DoRmAmMu1997 <80088665+DoRmAmMu1997@users.noreply.github.com>
2021-08-02 17:55:06 +0530

    Update ProfileResource.java

--- src/main/java/com/hemant/db/resource/ProfileResource.java
+++ src/main/java/com/hemant/db/resource/ProfileResource.java
@@ -3,8 +3,15 @@ package com.hemant.db.resource;
 import com.hemant.db.model.Profile;
 import com.hemant.db.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/rest/Profile")
@@ -19,32 +26,52 @@ public class ProfileResource {
     }
 
     @PostMapping(value = "/insert")
-    public List<Profile> persist(@RequestBody final Profile Profile) {
-        ProfileRepository.save(Profile);
-        return ProfileRepository.findAll();
+    public ResponseEntity<String> persist(@Valid @RequestBody final Profile Profile) {
+    	try {
+	    ProfileRepository.save(Profile);
+	    return ResponseEntity.ok("Success");
+    	} catch(Exception e) {
+    		return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    	}
     }
     
     @GetMapping("/findbygender")
-    public List<Profile> fetchDataByGender(@RequestParam("gender") String gender){        
+    public List<Profile> fetchDataByGender(@Valid @RequestParam("gender") String gender){        
         return ProfileRepository.findByGender(gender);
     }
     
     @GetMapping("/findbyhobbies")
-    public List<Profile> fetchDataByHobbies(@RequestParam("hobbies") String hobbies){        
+    public List<Profile> fetchDataByHobbies(@Valid @RequestParam("hobbies") String hobbies){        
         return ProfileRepository.findByHobbies(hobbies);
     }
     
     @PostMapping("/updatehobbies")
-    public List<Profile> updateFloor(@RequestParam("Id") Integer Id, @RequestParam("hobbies") String hobbies) {
-    	Profile p = ProfileRepository.findById(Id).get();
-    	p.setHobbies(hobbies);
-    	ProfileRepository.save(p);
-    	return ProfileRepository.findAll();
+    public ResponseEntity<String> updateHobbies(@RequestParam("Id") Integer Id, @Valid @RequestParam("hobbies") String hobbies) {
+	    Profile p = ProfileRepository.findById(Id).get();
+	    p.setHobbies(hobbies);
+	    ProfileRepository.save(p);
+	    return ResponseEntity.ok("Success");
     }
     
     @DeleteMapping("/delete")
-    public List<Profile> deleteProfile(@RequestParam("Id") Integer Id){
-    	ProfileRepository.deleteById(Id);
-    	return ProfileRepository.findAll();
+    public ResponseEntity<String> deleteProfile(@RequestParam("Id") Integer Id){
+	    try {
+	    	ProfileRepository.deleteById(Id);
+		    return ResponseEntity.ok("Success");
+	    } catch(Exception e) {
+	    	return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+	    }
+	}
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
