6f5485a51fcae6b575ffd566df34169740732c6d
DoRmAmMu1997 <80088665+DoRmAmMu1997@users.noreply.github.com>
2021-08-03 16:54:26 +0530

    Create IntegerValidator.java

---
+++ src/main/java/com/hemant/db/validators/IntegerValidator.java
@@ -0,0 +1,14 @@
+package com.hemant.db.validators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class IntegerValidator implements ConstraintValidator<IsInteger, Integer>{
+	public boolean isValid(Integer Id, ConstraintValidatorContext constraintValidatorContext) {
+        if(Id instanceof Integer) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
