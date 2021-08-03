9c1ef781b58d7a2af34bd66dde01b412d9d05252
DoRmAmMu1997 <80088665+DoRmAmMu1997@users.noreply.github.com>
2021-08-03 16:44:55 +0530

    Update AddressRepository.java

--- src/main/java/com/hemant/db/repository/AddressRepository.java
+++ src/main/java/com/hemant/db/repository/AddressRepository.java
@@ -15,10 +15,13 @@ public interface AddressRepository extends JpaRepository<Address, Integer> {
 	
 	public Optional<Address> findById(@Param("Id") Integer Id);
 	
-	@Modifying
-    @Query("update Address a set a.address1 = :address1, a.address2 = :address2, a.city = :city, a.state = :state, a.pin = :pin where a.Id = :Id")
-    public List<Address> updateAddress(@Param("Id") Integer Id, @Param("address1") String address1, @Param("address2") String address2, @Param("city") String city, @Param("state") String state, @Param("pin") Integer pin);
+	@Query("select a from Address a where a.empId = :empId")
+	public List<Address> findByEmp(@Param("empId") Integer empId);
 	
 	@Modifying
-    public void deleteById(@Param("Id") Integer Id);
+	@Query("update Address a set a.address1 = :address1, a.address2 = :address2, a.city = :city, a.state = :state, a.pin = :pin where a.Id = :Id")
+	public List<Address> updateAddress(@Param("Id") Integer Id, @Param("address1") String address1, @Param("address2") String address2, @Param("city") String city, @Param("state") String state, @Param("pin") Integer pin);
+
+	@Modifying
+	public void deleteById(@Param("Id") Integer Id);
 }
